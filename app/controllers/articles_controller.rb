43a7069f8cbd3306f06830b325a7e7e1a78e994a
Hudson-Pufferfish <hungson.nguyen811@gmail.com>
2022-06-13 12:19:52 +0700

    update the Article index page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id]) #Instance variable
   end
+
+  def index
+    @articles = Article.all
+  end
 end
