cb8655bd888d07212506f7172c310d76d2841611
Hudson-Pufferfish <hungson.nguyen811@gmail.com>
2022-07-01 12:13:19 +0700

    add admin function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_20_050040) do
+ActiveRecord::Schema.define(version: 2022_07_01_035553) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,7 +25,9 @@ ActiveRecord::Schema.define(version: 2022_06_20_050040) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "passsword_digest"
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
